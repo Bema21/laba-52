39c5f9f58e12dbffbe1403a0e7673897f5d3dbf5
Mederbekova Begimai <begimaialymbekova2@gmail.com>
2024-05-22 14:21:25 +0600

    1-level completed.

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,14 @@
-import './App.css'
+import './App.css';
 
 const App = () => {
-
   return (
-    <>
-
-    </>
-  )
+      <div className="playingCards faceImages">
+              <span className="card rank-k diams">
+             <span className="rank">K</span>
+             <span className="suit">♥</span>
+              </span>
+      </div>
+  );
 };
 
-export default App
+export default App;
