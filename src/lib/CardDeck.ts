a73b3f496e8ca9e71225e9e2ed3d1a80ace77ada
Mederbekova Begimai <begimaialymbekova2@gmail.com>
2024-05-22 15:26:19 +0600

    2-level completed.

---
+++ src/lib/CardDeck.ts
@@ -0,0 +1,30 @@
+import Card from "./Card.ts";
+
+export default class CardDeck {
+    public cards: Card [];
+
+    constructor() {
+        this.cards= [];
+        const ranks  = ['2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K', 'A'];
+        const suits = ['diams', 'hearts', 'clubs', 'spades'];
+
+        for (const rank of ranks) {
+            for (const suit of suits) {
+                this.cards.push(new Card(suit, rank));
+            }
+        }
+    }
+
+    getCard(): Card {
+        return this.cards.splice(Math.floor(Math.random()*this.cards.length), 1 )[0];
+    }
+
+
+    getCards(howMany:number): Card[]{
+        const cards: Card[] = [];
+        for (let i = 0; i < howMany; i++) {
+            cards.push(this.getCard());
+        }
+        return cards;
+    }
+}
